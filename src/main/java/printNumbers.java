0af2a84f7c93457d58a89911ceae96aba824d3ae
selfservice-test <vishalthakurtest1712@gmail.com>
2020-04-08 15:27:56 +0530

    code uploaded

---
+++ src/main/java/printNumbers.java
@@ -0,0 +1,12 @@
+public class printNumbers{
+	
+	public static void main(String args[]) {
+		System.out.println("HI there...");
+		for (int i=1;i<11;i++) {
+			
+			System.out.println("Printing number: "+i);
+			
+		}
+		System.out.println("Code completed...");
+	}
+}
