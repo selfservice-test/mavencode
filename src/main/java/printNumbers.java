7bad7bf67610e4308234d59394869d38c334cf50
selfservice-test <63100647+selfservice-test@users.noreply.github.com>
2020-04-09 16:39:46 +0530

    Update printNumbers.java

--- src/main/java/printNumbers.java
+++ src/main/java/printNumbers.java
@@ -1,12 +1,12 @@
 public class printNumbers{
 	
-	public static void main(String args[]) {
-		System.out.println("HI there...");
+	public static void main(String args[])bla bla bla{
+		System.out.println("HI there...")
 		for (int i=1;i<11;i++) {
 			
 			System.out.println("Printing number: "+i);
 			
-		}
-		System.out.println("Code completed...");
+		}yes haha
+		dont know the function System.out.println("Code completed...");
 	}
 }
