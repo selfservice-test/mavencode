fe60963c040943dcbb94b6ce671f52c442fa40c3
selfservice-test <63100647+selfservice-test@users.noreply.github.com>
2020-04-09 16:43:32 +0530

    Update printNumbers.java

--- src/main/java/printNumbers.java
+++ src/main/java/printNumbers.java
@@ -2,7 +2,7 @@ public class printNumbers{
 	// Code starts
 	// Code starts
 	public static void main(String args[]){
-		System.out.println("HI there...")
+		System.out.println("HI there...");
 		for (int i=1;i<11;i++) {
 			
 			System.out.println("Printing number: "+i);
